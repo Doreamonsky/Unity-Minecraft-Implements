4852a32394ac01a54250e7ad03f47d69d20d61cc
Wang Shijiang <403036847@qq.com>
2019-05-12 15:50:51 +0800

    Inventory System

---
+++ Assets/Core/Runtime/InventoryIconUI.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace MC.Core
+{
+    public class InventoryIconUI : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public static System.Action<int, int> OnSwapItem;
+
+        //Slot ID 由InventorySystem 设置
+        private int m_slotID = 0;
+
+        private Image m_icon;
+        private Vector3 m_origin_pos = Vector3.zero;
+        private Transform m_orign_parent;
+
+        public void Init(int slotID, Image icon)
+        {
+            m_slotID = slotID;
+            m_icon = icon;
+
+            m_origin_pos = m_icon.rectTransform.localPosition;
+            m_orign_parent = m_icon.transform.parent;
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            m_icon.raycastTarget = false;
+            m_icon.transform.parent = m_icon.transform.GetComponentInParent<Canvas>().transform;
+            m_icon.transform.SetAsLastSibling();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            m_icon.rectTransform.position = eventData.position;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            var rayHit = eventData.pointerCurrentRaycast;
+
+            Debug.Log(rayHit.gameObject, rayHit.gameObject);
+
+            var targetIconUI = rayHit.gameObject?.transform?.parent?.GetComponent<InventoryIconUI>();
+
+            if (targetIconUI != null)
+            {
+                OnSwapItem?.Invoke(m_slotID, targetIconUI.GetSlotID());
+            }
+
+            m_icon.transform.parent = m_orign_parent;
+            m_icon.rectTransform.localPosition = m_origin_pos;
+            m_icon.raycastTarget = true;
+        }
+
+        public int GetSlotID()
+        {
+            return m_slotID;
+        }
+
+
+    }
+
+}
---
+++ Assets/Core/Runtime/InventorySystem.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MC.Core
+{
+    public class InventorySystem : MonoBehaviour
+    {
+        public static int max_inventory_count = 10;
+
+        //Inventory UI
+        [System.Serializable]
+        public class UILayout
+        {
+            public class ItemUI
+            {
+                public Image itemIcon;
+
+                public GameObject instance;
+            }
+
+            public GameObject ItemTemplate;
+
+            public List<ItemUI> itemInstance = new List<ItemUI>();
+
+            public void Init()
+            {
+                for (var i = 0; i < max_inventory_count; i++)
+                {
+                    var instance = Object.Instantiate(ItemTemplate, ItemTemplate.transform.parent, true);
+
+                    var uiItem = new ItemUI()
+                    {
+                        instance = instance,
+                        itemIcon = instance.transform.Find("ItemIcon").GetComponent<Image>()
+                    };
+                    itemInstance.Add(uiItem);
+
+                    var iconUI = instance.AddComponent<InventoryIconUI>();
+                    iconUI.Init(i, uiItem.itemIcon);
+                }
+
+                ItemTemplate.gameObject.SetActive(false);
+            }
+        }
+
+
+        //玩家拥有物品的储存
+        //Slot ID 插槽  <= max_inventory_count 显示在底部
+        [System.Serializable]
+        public class InventoryStorage
+        {
+            public int count = 1;
+            public int slotID = -1;
+            public Inventory inventory;
+        }
+
+        public UILayout layout = new UILayout();
+
+        public List<InventoryStorage> inventoryStorageList = new List<InventoryStorage>();
+
+        private void Start()
+        {
+            layout.Init();
+
+            //交换物品
+            InventoryIconUI.OnSwapItem += (a, b) =>
+            {
+                var itemA = inventoryStorageList.Find(val => val.slotID == a);
+                var itemB = inventoryStorageList.Find(val => val.slotID == b);
+
+                if (itemA != null)
+                {
+                    itemA.slotID = b;
+                }
+
+                if (itemB != null)
+                {
+                    itemB.slotID = a;
+                }
+
+                UpdateInvetoryUI();
+            };
+
+            UpdateInvetoryUI();
+        }
+
+        private void UpdateInvetoryUI()
+        {
+            //更新底部插槽
+            for (var i = 0; i < max_inventory_count; i++)
+            {
+                var item = inventoryStorageList.Find(val => val.slotID == i);
+
+                if (item != null)
+                {
+                    layout.itemInstance[i].itemIcon.sprite = item.inventory.inventoryIcon;
+                }
+                else
+                {
+                    layout.itemInstance[i].itemIcon.sprite = null;
+                }
+            }
+        }
+
+    }
+
+}
