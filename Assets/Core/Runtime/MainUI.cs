e07c242c607220bf2b323f6b57904db9a6d2623b
Wang Shijiang <403036847@qq.com>
2019-05-26 14:32:34 +0800

    Add MainUI

---
+++ Assets/Core/Runtime/MainUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MC.Core
+{
+    public class MainUI : MonoBehaviour
+    {
+        public Button PlotBtn, SurvivalBtn, ExitBtn;
+
+        public GameObject LoadingEffect;
+
+        private void Start()
+        {
+            PlotBtn.onClick.AddListener(() =>
+            {
+                LoadingEffect.SetActive(true);
+                SceneManager.LoadScene("SampleScene");
+            });
+
+            SurvivalBtn.onClick.AddListener(() =>
+            {
+                LoadingEffect.SetActive(true);
+                SceneManager.LoadScene("InfiniteScene");
+            });
+            ExitBtn.onClick.AddListener(() =>
+            {
+                Application.Quit();
+            });
+
+        }
+    }
+
+}
