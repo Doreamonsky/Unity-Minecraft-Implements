1c537ad3f2f7a5bf960ecf9c2fc8ded99dca88e6
Wang Shijiang <403036847@qq.com>
2019-06-08 21:20:52 +0800

    Add solutions for ipad

--- Assets/Core/Runtime/MainUI.cs
+++ Assets/Core/Runtime/MainUI.cs
@@ -1,77 +1,84 @@
 ﻿using UnityEngine;
+#if UNITY_IOS
+using UnityEngine.iOS;
+#endif
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace MC.Core
-{
-    public class MainUI : MonoBehaviour
-    {
+namespace MC.Core {
+    public class MainUI : MonoBehaviour {
         public Button PlotBtn, SurvivalBtn, ExitBtn, CleanData;
 
         public Toggle TouchToggle;
 
         public GameObject LoadingEffect;
 
-        private void Start()
-        {
-            PlotBtn.onClick.AddListener(() =>
-            {
-                LoadingEffect.SetActive(true);
-                SceneManager.LoadScene("SampleScene");
+        private void Start () {
+            PlotBtn.onClick.AddListener (() => {
+                LoadingEffect.SetActive (true);
+                SceneManager.LoadScene ("SampleScene");
             });
 
-            SurvivalBtn.onClick.AddListener(() =>
-            {
-                LoadingEffect.SetActive(true);
-                SceneManager.LoadScene("InfiniteScene");
+            SurvivalBtn.onClick.AddListener (() => {
+                LoadingEffect.SetActive (true);
+                SceneManager.LoadScene ("InfiniteScene");
             });
 
-            ExitBtn.onClick.AddListener(() =>
-            {
-                Application.Quit();
+            ExitBtn.onClick.AddListener (() => {
+                Application.Quit ();
             });
 
-            TouchToggle.onValueChanged.AddListener(val =>
-            {
+            TouchToggle.onValueChanged.AddListener (val => {
                 Util.isTouchingScreen = val;
-                PlayerPrefs.SetString("Touching", val.ToString());
+                PlayerPrefs.SetString ("Touching", val.ToString ());
 
-                var text = TouchToggle.transform.Find("Label").GetComponent<Text>();
+                var text = TouchToggle.transform.Find ("Label").GetComponent<Text> ();
 
-                if (val)
-                {
+                if (val) {
                     text.text = "触屏控制";
-                }
-                else
-                {
+                } else {
                     text.text = "键鼠控制";
                 }
             });
 
-            var isTouching = PlayerPrefs.GetString("Touching", true.ToString()) == true.ToString();
+            var isTouching = PlayerPrefs.GetString ("Touching", true.ToString ()) == true.ToString ();
 
             TouchToggle.isOn = isTouching;
 
-            if (PlayerPrefs.HasKey("Plot"))
-            {
+            if (PlayerPrefs.HasKey ("Plot")) {
                 SurvivalBtn.interactable = true;
             }
 
-            CleanData.onClick.AddListener(() =>
-            {
-                PlayerPrefs.DeleteAll();
-                GeneralStorageSystem.DeleteFolder();
+            CleanData.onClick.AddListener (() => {
+                PlayerPrefs.DeleteAll ();
+                GeneralStorageSystem.DeleteFolder ();
             });
 
 #if UNITY_EDITOR
             SurvivalBtn.interactable = true;
-            SurvivalBtn.colors = new ColorBlock()
-            {
+            SurvivalBtn.colors = new ColorBlock () {
                 normalColor = Color.red
             };
 #else
             CleanData.interactable = false;
 #endif
+
+#if UNITY_IOS
+
+            var isIpad = Device.generation.ToString ().ToLower ().Contains ("ipad");
+
+            if (isIpad) {
+                var screenWidth = PlayerPrefs.GetInt ("ScreenWidth", Screen.width);
+                var screenHeight = PlayerPrefs.GetInt ("ScreenHeight", Screen.height);
+
+                if (!PlayerPrefs.HasKey ("ScreenWidth")) {
+                    PlayerPrefs.SetInt ("ScreenWidth", screenWidth);
+                    PlayerPrefs.SetInt ("ScreenHeight", screenHeight);
+                }
+                Screen.SetResolution (Mathf.RoundToInt (screenWidth * 0.6f), Mathf.RoundToInt (screenHeight * 0.6f), true);
+            }
+#endif
+
         }
     }
 
