ce74b83366ba0698ae289110755e8cf4fd09c58c
Wang Shijiang <403036847@qq.com>
2019-07-02 12:03:43 +0800

    Test AR

---
+++ Assets/AR/ARPlaceWorld.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+namespace MC.Core.AR
+{
+    public class ARPlaceWorld : MonoBehaviour
+    {
+        public Button placeWorldBtn;
+
+        public WorldManager worldManager;
+
+        public ARRaycastManager arRaycastManager;
+
+        private void Start()
+        {
+            placeWorldBtn.onClick.AddListener(() =>
+            {
+                var hits = new List<ARRaycastHit>();
+
+                arRaycastManager.Raycast(new Vector2(Screen.width * 0.5f, Screen.height * 0.5f), hits, TrackableType.All);
+
+                if (hits.Count > 0)
+                {
+                    worldManager.transform.position = hits[0].pose.position - Vector3.up * 15;
+                    worldManager.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
+                    placeWorldBtn.interactable = false;
+                }
+            });
+        }
+    }
+
+}
